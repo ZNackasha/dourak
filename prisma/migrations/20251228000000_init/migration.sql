3b353fa877ed0a802ec2139fee4bd2a30a6f29ac
ZNackasha <znackasha@gmail.com>
2025-12-27 20:20:36 -0500

    fix: comment out GRANT statements in migration script

--- prisma/migrations/20251228000000_init/migration.sql
+++ prisma/migrations/20251228000000_init/migration.sql
@@ -1,8 +1,8 @@
 -- Reset Database
 DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
+-- GRANT ALL ON SCHEMA public TO postgres;
+-- GRANT ALL ON SCHEMA public TO public;
 
 -- CreateEnum
 CREATE TYPE "ScheduleStatus" AS ENUM ('DRAFT', 'RECRUITMENT', 'SCHEDULED', 'ARCHIVED');
